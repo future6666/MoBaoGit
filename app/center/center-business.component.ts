2b443222ea0b2da3d6f20b508826d499e540eea6
Miccy <1157381556@qq.com>
2016-10-28 17:35:26 +0800

    添加了center文件夹

---
+++ app/center/center-business.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/10/28.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'center-business',
+    templateUrl: './center-business.component.html'
+})
+
+export class CenterBusinessComponent {
+
+}
---
+++ app/center/order-mine.component.ts
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/10/28.
+ */
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'order-mine',
+    templateUrl: './order-mine.component.html'
+})
+
+export class OrderMineComponent {
+
+}
